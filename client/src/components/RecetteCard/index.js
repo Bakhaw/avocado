38cc98e0fa9ab175dd457d392e741eab2893c4a7
Faissal <fhattou@simplon.co>
2018-11-08 16:44:29 +0100

    splitting ...

--- client/src/components/RecetteCard/index.js
+++ client/src/components/RecetteCard/index.js
@@ -1,29 +1,18 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 
 import CardActions from './CardActions';
 import CardHeader from './CardHeader';
 import CardMedia from './CardMedia';
 
-import { withContext } from '../../Context/AppStateProvider';
-
-class RecetteCard extends Component {
-
-  render() {
-    const { item } = this.props;
-
-    return (
-      <Fragment>
-        <Card className='recette-card'>
-          <CardHeader item={item} />
-
-          <CardMedia item={item} />
-
-          <CardActions item={item} />
-        </Card>
-      </Fragment>
-    );
-  }
+const RecetteCard = ({ item }) => {
+  return (
+    <Card className='recette-card'>
+      <CardHeader item={item} />
+      <CardMedia item={item} />
+      <CardActions item={item} />
+    </Card>
+  );
 }
 
-export default withContext(RecetteCard);
+export default RecetteCard;
