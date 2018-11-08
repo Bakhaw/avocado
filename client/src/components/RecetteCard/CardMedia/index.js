9181a469ccbc8089f8555070111dce29b9b69b79
Faissal <fhattou@simplon.co>
2018-11-08 14:53:56 +0100

    Splitted code to render only 'pure' components in <RecetteCard />

--- client/src/components/RecetteCard/CardMedia/index.js
+++ client/src/components/RecetteCard/CardMedia/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CardMedia from '@material-ui/core/CardMedia';
 
 const Media = ({ item }) => {
@@ -6,11 +7,14 @@ const Media = ({ item }) => {
     const image = `src/assets/images/recette-images/${recetteImage}`;
 
     return (
-        <CardMedia
-            image={image}
-            style={{ height: 0, paddingTop: '56.25%' }}
-            title={title}
-        />
+        <Link params={{ recetteId: item._id }}
+            to={`/recettes/${item._id}`}>
+            <CardMedia
+                image={image}
+                style={{ height: 0, paddingTop: '56.25%' }}
+                title={title}
+            />
+        </Link>
     )
 }
 
